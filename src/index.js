70fa71b64bcda97cc703c71cbdc0165e602a8169
AssaultMachete <assualtmachete20@gmail.com>
2023-09-12 12:42:37 +0300

    Bot frequent commits

---
+++ src/index.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+const { Client, IntentsBitField } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", (c) => {
+  console.log(`${client.user.username} is online!!!`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content === "segs") {
+    message.reply("https://i.imgur.com/Xh7OF5e.jpg");
+  }
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content === "gnite") {
+    message.reply(
+      "https://tenor.com/view/goodbye-chat-goodbye-chat-majima-majima-gif-21917631"
+    );
+  }
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content === "pink pilled") {
+    message.reply(
+      "https://media.discordapp.net/attachments/1139915622696898632/1150781240736231456/20230727_204911.jpg?width=956&height=1354"
+    );
+  }
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content === "me when loli") {
+    message.reply(
+      "https://tenor.com/view/genshin-impact-yakuza-yakuza0-kiryu-loli-gif-18947645"
+    );
+  }
+});
+
+client.login(process.env.token);
